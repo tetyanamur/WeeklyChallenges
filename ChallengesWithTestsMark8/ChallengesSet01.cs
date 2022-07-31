f1fbed1eeb01c8ceacf405c3b80b8f310cf3e07e
Tetyana Murzina <tetyanamur@gmail.com>
2022-07-31 00:32:12 -0500

    initial commit

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -9,8 +9,7 @@ namespace ChallengesWithTestsMark8
             
             public bool AreTwoNumbersTheSame(int number1, int number2)
             {
-                
-                if (number1 == number2)
+            if (number1 == number2)
                 {
                     return true;
                 }
@@ -20,53 +19,41 @@ namespace ChallengesWithTestsMark8
                 }
             }
 
-
             public double Subtract(double minuend, double subtrahend)
             {
-               
                 return minuend - subtrahend;
             }
 
-
             public int Add(int number1, int number2)
-
             {
                 return number1 + number2;
-
             }
+            
             public int GetSmallestNumber(int number1, int number2, int expected)
-            {
-
+            {   
                 if (number1 > number2)
                 {
-                    return number2;
-
+                  return number2;
                 }
                 else if (number2 > number1)
                 {
                     return number1;
                 }
-
-                else
+                 else
                 {
                     return expected;
                 }
             }
-
-
+            
             public long Multiply(long factor1, long factor2)
             {
                 return factor1 * factor2;
             }
 
             public string GetGreeting(string personName)
-
             {
                 return personName = "Tetyana";
-
-
             }
-
 
             public string GetHey()
             {

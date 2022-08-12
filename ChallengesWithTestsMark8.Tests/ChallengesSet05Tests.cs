922bc1ec90e396d46a9303e726ac1d893d0856a4
Tetyana Murzina <tetyanamur@gmail.com>
2022-08-11 22:18:44 -0500

    initial commit

--- ChallengesWithTestsMark8.Tests/ChallengesSet05Tests.cs
+++ ChallengesWithTestsMark8.Tests/ChallengesSet05Tests.cs
@@ -6,207 +6,120 @@ namespace ChallengesWithTestsMark8.Tests
 {
     public class ChallengesSet05Tests
     {
-        [Theory]
-        [InlineData(5, 9, 9)]
-        [InlineData(5, 2, 6)]
-        [InlineData(21, 20, 40)]
-        [InlineData(102, 5, 105)]
-        [InlineData(0, 7, 7)]
-        [InlineData(100, 5, 105)]
-        [InlineData(3, 6, 6)]
-        public void NextNumberDivisibleByN(int startNumber, int n, int expected)
+
+        public int NextNumberDivisibleByN(int startNumber, int n, int expected)
         {
-            // Arrange
-            ChallengesSet05 challenger = new ChallengesSet05();
-
-            // Act
-            int actual = challenger.GetNextNumberDivisibleByN(startNumber, n);
-
-            // Assert
-            Assert.Equal(expected, actual);
+            if (startNumber == 0)]
+            {
+                return n;
+            }
+            List<int> firstlist = new List<int>();
+            int maxLoop = startNumber * n;
+            int[] numberDivisiblebyN = new int[0];
+            for (int i = 0; i < maxLoop, i++)
+            {
+                if (i > startNumber && i % n == 0)
+                {
+                    result = 1;
+                    break;
+                }
+            }
+            return reuslt;
         }
 
-        [Theory]
-        [InlineData(new[] { "Test1", "Test2", "Test3", "Test4" }, new[] { 100d, 10d, 0d, 250d })]
-        [InlineData(new[] { "Test1", "Test2", "Test3", "Test4" }, new[] { 0.5d, 0d, 0d, 1d })]
-        [InlineData(new[] { "Test1", "Test2", "Test3", "Test4" }, new[] { 99d, 65d, 80d, 35d })]
-        [InlineData(new[] { "Test1", "Test2", "Test3", "Test4" }, new[] { 0d, 1d, 2d, 3d })]
-        [InlineData(new[] { "Test1", "Test2", "Test3" }, new[] { 22d, 33d, 44d })]
-        [InlineData(new[] { "Test1", "Test2", "Test3" }, new[] { 55d, 11d, 0d })]
-        [InlineData(new[] { "Test1" }, new[] { 100d })]
-        [InlineData(new[] { "Test1" }, new[] { 0d })]
-        public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(string[] names, double[] revenues)
+        public bool ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(string[] names, double[] revenues)
         {
             // Arrange
             ChallengesSet05 challenger = new ChallengesSet05();
             Business[] businesses = new Business[names.Length];
             for (int i = 0; i < names.Length; i++)
             {
-                businesses[i] = new Business()
+                if (businesses[i].TotalRevenue == 0)
                 {
-                    Name = names[i],
-                    TotalRevenue = revenues[i]
+                    businesses[i].Name = "CLOSED";
                 };
             }
+        }
+        // Act
 
-            // Act
-            challenger.ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(businesses);
-
-            // Assert
-            foreach (Business business in businesses)
+        public int IsAscendingOrder(int[] nums, bool expected)
+        { if (numbers == null || numbers.length == 0)
             {
-                if (business.TotalRevenue == 0)
+                return false;
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (numbers[i] < numbers[i - 1])
                 {
-                    Assert.Equal("CLOSED", business.Name);
+                    return false;
                 }
-                else
-                {
-                    Assert.NotEqual("CLOSED", business.Name);
-                }
+            }
+            return true;
+        }
+
+
+        public int SumElementsThaFollowAnEven(int[] numbers, int expected)
+        {
+            if (numbers == null || numbers.length == 0)
+            {
+                sum += numbers[i];
+            }
+        }
+            return sum;
+    }
+    public string TurnWordsIntoSentence(string[] words, string expected)
+    {
+        if (words == null || words.Length == 0)
+        {
+            return "";
+        }
+
+        string sentence = "";
+
+        foreach (string word in words)
+        {
+            if (word.Trim().Length > 0)
+            {
+                sentence += word.Trim() + " ";
             }
         }
 
-        [Theory]
-        [InlineData(new[] { 1, 2, 3 }, true)]
-        [InlineData(new[] { -3, -2, -1 }, true)]
-        [InlineData(new[] { 1, 2, 3, 4, 5, 6 }, true)]
-        [InlineData(new[] { 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 6, 6, 6 }, true)]
-        [InlineData(new[] { 10, 1000, 1002 }, true)]
-        [InlineData(new[] { -1000, -1000, -500, 0, 1000000 }, true)]
-        [InlineData(new[] { 10 }, true)]
-        [InlineData(new[] { 3, 2, 1 }, false)]
-        [InlineData(new[] { 0, -1, -2 }, false)]
-        [InlineData(new[] { 1, 2, 3, 2 }, false)]
-        [InlineData(new[] { 1, 2, 4, 3, 5 }, false)]
-        [InlineData(new[] { 1, 1, 2, 2, 3, 4, 4, 3 }, false)]
-        [InlineData(new[] { -1000, -500, -500, 10000000, 1000000 }, false)]
-        [InlineData(new[] { 5, 2, 3, 4 }, false)]
-        [InlineData(new[] { 1, 0 }, false)]
-        [InlineData(new int[] { }, false)]
-        [InlineData(null, false)]
-        public void IsAscendingOrder(int[] nums, bool expected)
+        if (sentence.Length == 0)
         {
-            // Arrange
-            ChallengesSet05 challenger = new ChallengesSet05();
-
-            // Act
-            bool actual = challenger.IsAscendingOrder(nums);
-
-            // Assert
-            Assert.Equal(expected, actual);
+            return "";
         }
 
-        [Theory]
-        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 8)]
-        [InlineData(new int[] { -1, -2, -3, -4, -5 }, -8)]
-        [InlineData(new int[] { 1, 2, 2, 3, 4 }, 5)]
-        [InlineData(new int[] { 1, -2, -2, 3, -4 }, 1)]
-        [InlineData(new int[] { 1, -2, 2, 3, 4 }, 5)]
-        [InlineData(new int[] { 1, 3, 5, 7 }, 0)]
-        [InlineData(new int[] { 1, 3, 5, 8 }, 0)]
-        [InlineData(new int[] { 10, 10, 10, 10 }, 30)]
-        [InlineData(new int[] { 0, 1, 0 }, 1)]
-        [InlineData(new int[] { }, 0)]
-        [InlineData(null, 0)]
-        public void SumElementsThaFollowAnEven(int[] numbers, int expected)
+        sentence = sentence.Substring(0, sentence.Length - 1);
+        sentence += ".";
+        return sentence;
+    }
+    public double GetEveryFourthElement(double[] numbers, double[] expected)
+    {
+        if (numbers == null || elements.count == 0)
         {
-            // Arrange
-            ChallengesSet05 challenger = new ChallengesSet05();
-
-            // Act
-            int actual = challenger.SumElementsThatFollowAnEven(numbers);
-
-            // Assert
-            Assert.Equal(expected, actual);
+            return new double[0];
         }
-
-        [Theory]
-        [InlineData(new string[] { "Hello,", "world" }, "Hello, world.")]
-        [InlineData(new string[] { "This", "is", "only", "a", "test" }, "This is only a test.")]
-        [InlineData(new string[] { "This", "is", "", "a", "test" }, "This is a test.")]
-        [InlineData(new string[] { "This", "is", " ", "a", "test" }, "This is a test.")]
-        [InlineData(new string[] { "This", "is ", "  ", "a  ", " test  " }, "This is a test.")]
-        [InlineData(new string[] { " ", " " }, "")]
-        [InlineData(new string[] { }, "")]
-        [InlineData(null, "")]
-        public void TurnWordsIntoSentence(string[] words, string expected)
+        List<double> everyFourth = new List<double>();
+        for (int i = 3; i < elements.Count; i += 4)
         {
-            // Arrange
-            ChallengesSet05 challenger = new ChallengesSet05();
-
-            // Act
-            string actual = challenger.TurnWordsIntoSentence(words);
-
-            // Assert
-            Assert.Equal(expected, actual);
+            everyFourth.Add(elements[i]);
         }
+        return everyFourth.ToArray();
+    }
 
-        [Theory]
-        [InlineData(new[] { 1d, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 }, new[] { 4d, 8, 12, 16 })]
-        [InlineData(new[] { 1d, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }, new[] { 4d, 8, 12 })]
-        [InlineData(new[] { 1d, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, new[] { 4d, 8, 12 })]
-        [InlineData(new[] { 1d, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 }, new[] { 4d, 8 })]
-        [InlineData(new[] { 1d, 2, 3, 4, 5, 6, 7, 8 }, new[] { 4d, 8 })]
-        [InlineData(new[] { 1d, 2, 3, 4, 5, 6, 7 }, new[] { 4d })]
-        [InlineData(new[] { 1d, 2, 3, 4 }, new[] { 4d })]
-        [InlineData(new[] { 1d, 2, 3 }, new double[] { })]
-        [InlineData(new[] { 1d }, new double[] { })]
-        [InlineData(new double[] { }, new double[] { })]
-        [InlineData(null, new double[] { })]
-        public void GetEveryFourthElement(double[] numbers, double[] expected)
+    public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] numbers, int targetNumber, bool expected)
+    {
+        for (int i = 0; i < numbers.Length; i++)
         {
-            // Arrange
-            ChallengesSet05 challenger = new ChallengesSet05();
-            List<double> numbersAsList = numbers == null ? null : new List<double>(numbers);
-
-            // Act
-            double[] actual = challenger.GetEveryFourthElement(numbersAsList);
-
-            // Assert
-            Assert.Equal(expected.Length, actual.Length);
-            for (int i = 0; i < expected.Length; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
-                Assert.Equal(Math.Round(expected[i], 4), Math.Round(actual[i], 4));
+                if (nums[i] + nums[k] == targetNumber)
             }
+            return true;
         }
-
-        [Theory]
-        [InlineData(new[] { 1, 2, 3 }, 5, true)]
-        [InlineData(new[] { 1, 2, 3 }, 3, true)]
-        [InlineData(new[] { 7, 3, 20 }, 10, true)]
-        [InlineData(new[] { 7, 3, 20 }, 27, true)]
-        [InlineData(new[] { 5, -2, 4, 6, 8 }, 3, true)]
-        [InlineData(new[] { 5, -2, 4, 6, 8 }, 9, true)]
-        [InlineData(new[] { 5, -2, 4, 6, 8 }, 10, true)]
-        [InlineData(new[] { 5, -2, 4, 6, 8 }, 14, true)]
-        [InlineData(new[] { 5, -2, 4, 6, 8 }, 2, true)]
-        [InlineData(new[] { 5, -2, 4, 6, 8 }, 4, true)]
-        [InlineData(new[] { 5, -2, 4, 4, 8 }, 8, true)]
-        [InlineData(new[] { 5, -2, 4, 4 }, 8, true)]
-        [InlineData(new[] { 3, -3 }, 0, true)]
-        [InlineData(new[] { 3, -3, 3, -3 }, -6, true)]
-        [InlineData(new[] { 3, -3, 3, -3 }, 0, true)]
-        [InlineData(new[] { 3, -3, 3, -3 }, 6, true)]
-        [InlineData(new[] { 5, -2, 4, 6, 8 }, 20, false)]
-        [InlineData(new[] { 5, -2, 4, 6, 8 }, 5, false)]
-        [InlineData(new[] { 5, -2, 4, 6, 8 }, 8, false)]
-        [InlineData(new[] { 5, -2, 4, 6, 8 }, 15, false)]
-        [InlineData(new[] { 5, -2, 4, 6, 8 }, 0, false)]
-        [InlineData(new[] { 5, -2, 4, 6, 8 }, -2, false)]
-        [InlineData(new[] { 4 }, 8, false)]
-        [InlineData(new[] { 4 }, 4, false)]
-        [InlineData(new int[] { }, 1, false)]
-        [InlineData(new int[] { }, 0, false)]
-        public void TwoDifferentElementsInArrayCanSumToTargetNumber(int[] numbers, int targetNumber, bool expected)
-        {
-            // Arrange
-            ChallengesSet05 challenger = new ChallengesSet05();
-
-            // Act
-            bool actual = challenger.TwoDifferentElementsInArrayCanSumToTargetNumber(numbers, targetNumber);
-
-            // Assert
-            Assert.Equal(expected, actual);
+    }
+}
+    return false;
         }
     }
 }
